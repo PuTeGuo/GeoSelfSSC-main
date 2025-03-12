8f03115d285884f598fe4090df1b1e71b81b36de
ScarfRed <76199104+PuTeGuo@users.noreply.github.com>
2025-03-12 23:14:56 +0800

    Update plotting.py

--- utils/plotting.py
+++ utils/plotting.py
@@ -15,7 +15,6 @@ def draw_bbox(im, size):
     im[:, :, h2:h2+size, w2+size] = marker
     return im
 
-
 def plot_image_grid(images, rows, cols, directions=None, imsize=(2, 2), title=None, show=True):
     fig, axs = plt.subplots(rows, cols, gridspec_kw={'wspace': 0, 'hspace': 0}, squeeze=True, figsize=(rows * imsize[0], cols * imsize[1]))
     for i, image in enumerate(images):
@@ -30,13 +29,11 @@ def plot_image_grid(images, rows, cols, directions=None, imsize=(2, 2), title=No
         plt.show()
     return fig
 
-
 def show_save(save_path, show=True, save=False):
     if show:
         plt.show()
     if save:
         plt.savefig(save_path)
-
 
 def color_tensor(tensor: torch.Tensor, cmap, norm=False):
     if norm:
